2f05a7cb183925ff9cf7c016c6ce8b2cf97e43a6
Alejandra Juárez <134227700+Rebldomakr0@users.noreply.github.com>
2024-02-15 10:26:30 -0600

    Update recognition.js

--- recognition.js
+++ recognition.js
@@ -1,18 +1,1 @@
-const recognition = new webkitSpeechRecognition();
 
-recognition.onresult = function (event) {
-  const command = event.results[0][0].transcript.toLowerCase();
-
-  if (command.includes('play')) {
-    videoPlayer.play();
-  } else if (command.includes('pause')) {
-    videoPlayer.pause();
-  } else if (command.includes('stop')) {
-    videoPlayer.pause();
-    videoPlayer.currentTime = 0;
-  } else if (command.includes('next')) {
-    playNextVideo();
-  }
-};
-
-recognition.start();
